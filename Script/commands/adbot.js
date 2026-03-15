297e46790ef13ed8680b0e03a7a40b205dd0e568
jubayerjubu <jubayerislamjubu4@gmail.com>
2026-03-15 16:38:41 +0300

    Update admin info in adbot.js

--- Script/commands/adbot.js
+++ Script/commands/adbot.js
@@ -54,7 +54,7 @@ module.exports.run = async ({ api, event, args }) => {
     if (args[0] == "admin") {
         let callback = () => api.sendMessage(
             {
-                body: `———»ADMIN BOT«———\n❯ Name: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔\n❯ Facebook: https://facebook.com/100044713412032\n❯ Thanks for using ${global.config.BOTNAME} bot`,
+                body: `———»ADMIN BOT«———\n❯ Name: 𝐉𝐮𝐛𝐚𝐲𝐞𝐫 𝐁𝐚𝐛𝐲\n❯ Facebook: https://facebook.com/100064028587522\n❯ Thanks for using ${global.config.BOTNAME} bot`,
                 attachment: fs.createReadStream(__dirname + "/cache/1.png")
             },
             event.threadID,
